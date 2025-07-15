f1f2635501c087f7280ad39af47e5174549c8894
JRStudio1487 <jrdesarrollo@hotmail.com>
2025-07-15 11:32:05 +0200

    Iniciando proyecto

---
+++ src/main/java/es/jrstudio/conga/CongaBackendApplication.java
@@ -0,0 +1,13 @@
+package es.jrstudio.conga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/jrstudio/conga/CongaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jrstudio.conga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
